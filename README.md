85babaabb545a596d95afd074386e41119b329f5
Paulo Machado <pmachado@titanhq.com>
2025-11-06 17:08:33 +0000

    Add improving-fashion-classifier-with-convolutions

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # DeepLearning.AI TensorFlow Developer Professional Certificate
 
+You can find and download all the notebooks in this 
+public Github repository [GitHub - deeplearning.ai/tensorflow](https://github.com/https-deeplearning-ai/tensorflow-1-public)
+
+
+## Instructions
+
+Creating env. And follow the instructions.
+
+```bash
+mkvenv
+```
+
+Otherwise:
+
+Install Project Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+
 ## Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
 
 ## Convolutional Neural Networks in TensorFlow
---
+++ course-exercises/week3/improving-fashion-classifier-with-convolutions/main.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+def main():
+    # Load the Fashion MNIST dataset
+    fmnist = tf.keras.datasets.fashion_mnist
+    (training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+    # Normalize the pixel values
+    training_images = training_images / 255.0
+    test_images = test_images / 255.0
+
+    model = tf.keras.models.Sequential([
+        # Add convolutions and max pooling
+        tf.keras.Input(shape=(28,28,1)),
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        # Add the same layers as before
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    # Print the model summary
+    model.summary()
+
+    # Use same settings
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Train the model
+    print("\nMODEL TRAINING:")
+    model.fit(training_images, training_labels, epochs=5)
+
+    # Evaluate on the test set
+    print("\nMODEL EVALUATION:")
+    test_loss, test_accuracy = model.evaluate(test_images, test_labels, verbose=0)
+    print(f'test set accuracy: {test_accuracy}')
+    print(f'test set loss: {test_loss}')
+
+    print(f"First 100 labels:\n\n{test_labels[:100]}")
+
+    print(f"\nShoes: {[i for i in range(100) if test_labels[:100][i]==9]}")
+
+    FIRST_IMAGE=0
+    SECOND_IMAGE=23
+    THIRD_IMAGE=28
+    CONVOLUTION_NUMBER = 1
+    layers_to_visualize = [tf.keras.layers.Conv2D, tf.keras.layers.MaxPooling2D]
+
+    layer_outputs = [layer.output for layer in model.layers if type(layer) in layers_to_visualize]
+    activation_model = tf.keras.models.Model(inputs = model.inputs, outputs=layer_outputs)
+
+    f, axarr = plt.subplots(3,len(layer_outputs))
+
+    for x in range(len(layer_outputs)):
+        f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1), verbose=False)[x]
+        axarr[0,x].imshow(f1[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+        axarr[0,x].grid(False)
+    
+        f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1), verbose=False)[x]
+        axarr[1,x].imshow(f2[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+        axarr[1,x].grid(False)
+    
+        f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1), verbose=False)[x]
+        axarr[2,x].imshow(f3[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+        axarr[2,x].grid(False)
+
+
+if __name__ == "__main__":
+    main()
